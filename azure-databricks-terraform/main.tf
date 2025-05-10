2afefaa8483b33919c0a48e777cb330b3b042df1
monkinng9 <T_chayut@kkumail.com>
2025-05-10 17:10:39 +0700

    feat: add Databricks public and private subnet configurations with variables

--- azure-databricks-terraform/main.tf
+++ azure-databricks-terraform/main.tf
@@ -10,3 +10,31 @@ resource "azurerm_virtual_network" "vnet" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
+
+resource "azurerm_subnet" "databricks_public" {
+  name                 = "${var.prefix}-db-public-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.databricks_public_subnet_address_prefix
+  delegation {
+    name = "databricks"
+    service_delegation {
+      name    = "Microsoft.Databricks/workspaces"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
+
+resource "azurerm_subnet" "databricks_private" {
+  name                 = "${var.prefix}-db-private-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = var.databricks_private_subnet_address_prefix
+  delegation {
+    name = "databricks"
+    service_delegation {
+      name    = "Microsoft.Databricks/workspaces"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/join/action"]
+    }
+  }
+}
--- azure-databricks-terraform/variables.tf
+++ azure-databricks-terraform/variables.tf
@@ -21,3 +21,39 @@ variable "vnet_address_space" {
   type        = list(string)
   default     = ["10.0.0.0/16"]
 }
+
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+  default     = "learn_databricks"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "East US" # Choose a region that supports Premium Databricks & Unity Catalog
+}
+
+variable "prefix" {
+  description = "A prefix for all resource names to ensure uniqueness."
+  type        = string
+  default     = "ldb" # learn_databricks
+}
+
+variable "vnet_address_space" {
+  description = "The address space for the Virtual Network."
+  type        = list(string)
+  default     = ["10.0.0.0/16"]
+}
+
+variable "databricks_public_subnet_address_prefix" {
+  description = "Address prefix for Databricks public subnet."
+  type        = list(string)
+  default     = ["10.0.1.0/24"]
+}
+
+variable "databricks_private_subnet_address_prefix" {
+  description = "Address prefix for Databricks private subnet."
+  type        = list(string)
+  default     = ["10.0.2.0/24"]
+}
