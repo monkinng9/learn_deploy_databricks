954ab23e59fa9574d1cf4a49e0e69c79d9faf170
monkinng9 <T_chayut@kkumail.com>
2025-05-10 16:53:21 +0700

    feat: initialize Azure resource group with Terraform configuration

---
+++ azure-databricks-terraform/variables.tf
@@ -0,0 +1,11 @@
+variable "resource_group_name" {
+  description = "The name of the resource group."
+  type        = string
+  default     = "learn_databricks"
+}
+
+variable "location" {
+  description = "The Azure region where resources will be created."
+  type        = string
+  default     = "eastasia" # Choose a region that supports Premium Databricks & Unity Catalog
+}
